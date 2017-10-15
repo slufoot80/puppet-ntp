da33e39e403362d8ad7497e8ba016ec3f7fdc00d
root <root@slufoot801.mylabserver.com>
2017-10-15 21:35:16 +0000

    Update Commit October-15-2017

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,15 +5,17 @@ class ntp(
   String $config_name         = $ntp::params::config_name,
   String $config_file_mode    = $ntp::params::config_file_mode,
 
-  String $service_ensure      = $ntp::service_ensure,
+  String $service_ensure      = $ntp::params::service_ensure,
   String $service_name        = $ntp::params::service_name,
+
   Array[String] $servers      = $ntp::params::servers,
+
   Boolean $service_enable     = $ntp::params::service_enable,
   Boolean $service_hasrestart = $ntp::params::service_hasrestart,
   Boolean $service_hasstatus  = $ntp::params::service_hasstatus,
-) inherits ::ntp::params {
-  class { '::ntp::install':}
-  -> class { '::ntp::config':}
-  ~> class { '::ntp::service':}
 
+) inherits ::ntp::params {
+  class { '::ntp::install': }
+  -> class { '::ntp::config': }
+  ~> class { '::ntp::service': }
 }
